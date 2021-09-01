fc2c310a85175597b0e7f3a7bf33ed74a0da88e6
leonardobarone <leonardobarone26@gmail.com>
2021-09-01 16:35:31 +0200

    avanzamento esercizio

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
-alert("ciaone");
-
 // Chiedere all'utente di inserire il numero di celle di cui sarà composto il campo da gioco.
+var numeroDiCelle = parseInt(prompt("Inserisci il numero di celle di cui sarà composto il campo da gioco."));
+
+// Minimo di controllo
+while (isNaN(numeroDiCelle)) {
+    var numeroDiCelle = parseInt(prompt("INSERISCI UN NUMERO"));
+}
+
 // Tramite una funzione javascript disegnare in pagina la griglia con massimo 10 celle per riga.
+function disegnaTabella(num) {
+    
+    for (var i = 1; i <= num; i++) {
+        document.getElementById("campo-da-gioco").innerHTML += `<div class="quadrato"> ${i} </div>`
+    }
+
+}
+disegnaTabella(numeroDiCelle);
+
 // Al click su una cella dovrà essere mostrato con un alert il numero della cella e il suo background diventerà rosso.
